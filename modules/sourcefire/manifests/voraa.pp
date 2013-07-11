b0b1a42a332c76d4a42b9e84ce1795c6a5220b02
Nate <jfa@packetdamage.com>
2013-07-11 18:04:35 -0400

    sourcefire::voraa

--- modules/sourcefire/manifests/voraa.pp
+++ modules/sourcefire/manifests/voraa.pp
@@ -1,4 +1,16 @@
 class sourcefire::voraa {
+  github autoconf
+  github automake
+  github chrome  
+  github cmake   
+  github firefox 
+  github geoip   
+  github iterm2  
+  github php     
+  github python  
+  github macvim  
+  github wget    
+
   include autoconf
   include automake
   include chrome  
