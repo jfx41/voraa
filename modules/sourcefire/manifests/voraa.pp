00477cbc97bf44e2ce74e3f735edbcfabc2419d2
Nate <jfa@packetdamage.com>
2013-07-11 17:39:35 -0400

    Testing out Sourcefire module for Voraa

---
+++ modules/sourcefire/manifests/voraa.pp
@@ -0,0 +1,15 @@
+class sfvrt::voraa {
+  boxen::sfvrt { 'voraa':
+    autoconf      => true,
+    automake      => true,
+    chrome        => true,
+    cmake         => true,
+    firefox       => true,
+    geoip         => true,
+    iterm2        => true,
+    php           => true,
+    python        => true,
+    macvim        => true,
+    wget          => true,
+  }
+}
